049895cb74cc467d7e3d9eb248029166b15b519e
MilanBuday <238915@vutbr.cz>
2023-04-04 21:16:00 +0200

    Missing picture

--- 08-traffic_lights/traffic/traffic.srcs/sources_1/new/top.vhd
+++ 08-traffic_lights/traffic/traffic.srcs/sources_1/new/top.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 29.03.2023 15:47:27
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
